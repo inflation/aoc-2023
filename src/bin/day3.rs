64d1ed4360232b7c6c5299e5adca6630f2638281
Inflation <2375962+inflation@users.noreply.github.com>
2023-12-03 12:57:11 -0500

    Day 3 Part 2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,55 +1,116 @@
 use color_eyre::eyre::eyre;
+use log::info;
 
 #[aoc_macro::main("day3")]
 fn main() -> color_eyre::Result<u32> {
-    let re = regex::bytes::Regex::new(r"\d+")?;
+    let re_star = regex::bytes::Regex::new(r"\*")?;
+    let re_num = regex::bytes::Regex::new(r"\d+")?;
+
     let mat: Vec<_> = input.split(|&c| c == b'\n').collect();
     let m = mat.len();
     let n = mat[0].len();
 
+    let parts: Vec<Vec<_>> = mat
+        .iter()
+        .map(|line| re_num.find_iter(line).collect())
+        .collect();
+
     let mut sum = 0;
     for (i, line) in mat.iter().enumerate() {
-        for num in re.find_iter(line) {
-            let start = num.start();
-            let end = num.end();
-            let num: u32 = atoi_simd::parse(num.as_bytes()).map_err(|e| eyre!("{e}"))?;
+        for gear in re_star.find_iter(line) {
+            let gear = gear.start();
+            let mut count = 0;
+            let mut ratio = 1;
 
-            if start > 0 && line[start - 1] != b'.' || end < n - 1 && line[end] != b'.' {
-                println!("{}: left or right: {start}-{end}: {}", i + 1, num,);
-                sum += num;
-            } else {
-                for j in start..end {
-                    if i > 0 && mat[i - 1][j] != b'.' {
-                        println!("{}: top: {start}-{end}: {}", i + 1, num);
-                        sum += num;
-                        break;
-                    }
-                    if i < mat.len() - 1 && mat[i + 1][j] != b'.' {
-                        println!("{}: bottom: {start}-{end}: {}", i + 1, num);
-                        sum += num;
-                        break;
-                    }
-                    if i > 0 && j > 0 && mat[i - 1][j - 1] != b'.' {
-                        println!("{}: top left: {start}-{end}: {}", i + 1, num);
-                        sum += num;
-                        break;
-                    }
-                    if i < m - 1 && j > 0 && mat[i + 1][j - 1] != b'.' {
-                        println!("{}: bottom left: {start}-{end}: {}", i + 1, num);
-                        sum += num;
-                        break;
-                    }
-                    if i > 0 && j < n - 1 && mat[i - 1][j + 1] != b'.' {
-                        println!("{}: top right: {start}-{end}: {}", i + 1, num);
-                        sum += num;
-                        break;
-                    }
-                    if i < m - 1 && j < n - 1 && mat[i + 1][j + 1] != b'.' {
-                        println!("{}: bottom right: {start}-{end}: {}", i + 1, num);
-                        sum += num;
-                        break;
-                    }
+            if gear > 0 && line[gear - 1].is_ascii_digit() {
+                info!("{}: left: {gear}", i + 1,);
+                count += 1;
+                let part: u32 = atoi_simd::parse(
+                    parts[i]
+                        .iter()
+                        .find(|&m| m.end() == gear)
+                        .ok_or_else(|| eyre!("Parts not found: {} at line {}", gear - 1, i + 1))?
+                        .as_bytes(),
+                )
+                .map_err(|e| eyre!("{e}"))?;
+                ratio *= part;
+            }
+            if gear < n - 1 && line[gear + 1].is_ascii_digit() {
+                info!("{}: right: {gear}", i + 1,);
+                count += 1;
+                let part: u32 = atoi_simd::parse(
+                    parts[i]
+                        .iter()
+                        .find(|&m| m.start() == gear + 1)
+                        .ok_or_else(|| eyre!("Parts not found: {} at line {}", gear + 1, i + 1))?
+                        .as_bytes(),
+                )
+                .map_err(|e| eyre!("{e}"))?;
+                ratio *= part;
+            }
+            if i > 0 && mat[i - 1][gear].is_ascii_digit() {
+                if let Some(part) = parts[i - 1].iter().find(|&m| m.range().contains(&gear)) {
+                    info!("{}: up: {gear}", i + 1);
+                    count += 1;
+                    let part: u32 = atoi_simd::parse(part.as_bytes()).map_err(|e| eyre!("{e}"))?;
+                    ratio *= part;
+                } else {
+                    info!("Part not found: up");
                 }
+            }
+            if i < n - 1 && mat[i + 1][gear].is_ascii_digit() {
+                if let Some(part) = parts[i + 1].iter().find(|&m| m.range().contains(&gear)) {
+                    info!("{}: down: {gear}", i + 1,);
+                    count += 1;
+                    let part: u32 = atoi_simd::parse(part.as_bytes()).map_err(|e| eyre!("{e}"))?;
+                    ratio *= part;
+                } else {
+                    info!("Part not found: down");
+                }
+            }
+            if gear > 0 && i > 0 && mat[i - 1][gear - 1].is_ascii_digit() {
+                if let Some(part) = parts[i - 1].iter().find(|&m| m.end() == gear) {
+                    info!("{}: up-left: {gear}", i + 1,);
+                    count += 1;
+                    let part: u32 = atoi_simd::parse(part.as_bytes()).map_err(|e| eyre!("{e}"))?;
+                    ratio *= part;
+                } else {
+                    info!("Part not found: up-left");
+                }
+            }
+            if gear < n - 1 && i > 0 && mat[i - 1][gear + 1].is_ascii_digit() {
+                if let Some(part) = parts[i - 1].iter().find(|&m| m.start() == gear + 1) {
+                    info!("{}: up-right: {gear}", i + 1,);
+                    count += 1;
+                    let part: u32 = atoi_simd::parse(part.as_bytes()).map_err(|e| eyre!("{e}"))?;
+                    ratio *= part;
+                } else {
+                    info!("Part not found: up-right");
+                }
+            }
+            if gear > 0 && i < m - 1 && mat[i + 1][gear - 1].is_ascii_digit() {
+                if let Some(part) = parts[i + 1].iter().find(|&m| m.end() == gear) {
+                    info!("{}: down-left: {gear}", i + 1,);
+                    count += 1;
+                    let part: u32 = atoi_simd::parse(part.as_bytes()).map_err(|e| eyre!("{e}"))?;
+                    ratio *= part;
+                } else {
+                    info!("Part not found: down-left");
+                }
+            }
+            if gear < n - 1 && i < m - 1 && mat[i + 1][gear + 1].is_ascii_digit() {
+                if let Some(part) = parts[i + 1].iter().find(|&m| m.start() == gear + 1) {
+                    info!("{}: down-right: {gear}", i + 1,);
+                    count += 1;
+                    let part: u32 = atoi_simd::parse(part.as_bytes()).map_err(|e| eyre!("{e}"))?;
+                    ratio *= part;
+                } else {
+                    info!("Part not found: down-right");
+                }
+            }
+
+            if count == 2 {
+                sum += ratio;
             }
         }
     }
